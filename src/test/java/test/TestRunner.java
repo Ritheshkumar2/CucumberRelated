b70dc8905336fe683e038d4071437357f2367b9b
Ritheshkumar2 <ritheshbrahmanapally@gmail.com>
2025-06-22 09:00:17 +0530

    included main classess

--- src/test/java/test/TestRunner.java
+++ src/test/java/test/TestRunner.java
@@ -3,9 +3,9 @@ package test;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/main/java/featurefil",
+@CucumberOptions(features="src/main/java/featurefile",
 glue="stepdef",
-tags="@tag",
+tags="@tag12",
 monochrome=true,plugin= {"pretty","html:target/cucumber-report.html"})
 public class TestRunner extends AbstractTestNGCucumberTests{
 
